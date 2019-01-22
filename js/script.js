f0a53fb3697a37fe291d7ad2940b3332b45975d5
Inf1n8 <vikrame1999@gmail.com>
2019-01-22 18:43:58 +0530

    added animations for desktop layout

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ $('#fullpage').fullpage({
     verticalCentered: true,
     navigation:true,
     navigationPosition: 'right',
-    anchors:['one', 'two', 'three', 'four', 'five']
+    anchors:['one', 'two', 'three', 'four', 'five'],
+    afterLoad: function(anchorLink, index){
+    var slideNumber = $('.fp-section.active').find('.fp-slide.active').index() + 1
+        alert(slideNumber);
+}
 });
 // $("#clicks").on('click',function() {
 //     $('html, body').animate({
